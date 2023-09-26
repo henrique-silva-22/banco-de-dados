ec2a007a9e5d3d9f9d457667490b28df8a960d9e
henrique-silva-22 <102774904+henrique-silva-22@users.noreply.github.com>
2023-09-26 10:12:57 -0300

    Update listaExercicios02.sql
    commit 2

--- parte2/listaExercicios02.sql
+++ parte2/listaExercicios02.sql
@@ -5,3 +5,17 @@ BEGIN
     SELECT * FROM Autor;
 END //
 DELIMITER ;
+
+2)  Livros por Categoria:
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+DELIMITER ;
+
+-- Teste a stored procedure
+CALL sp_LivrosPorCategoria('Romance');

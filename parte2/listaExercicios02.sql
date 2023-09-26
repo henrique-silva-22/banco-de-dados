16f97b7344973ca68525f871fa14ecd042517a5f
henrique-silva-22 <102774904+henrique-silva-22@users.noreply.github.com>
2023-09-26 10:18:40 -0300

    Update listaExercicios02.sql
    commit4

--- parte2/listaExercicios02.sql
+++ parte2/listaExercicios02.sql
@@ -21,7 +21,7 @@ DELIMITER ;
 -- Teste a stored procedure
 CALL sp_LivrosPorCategoria('Romance');
 
-3) Contagem de Livros por Categoria
+3) Contagem de Livros por Categoria:
 
 DELIMITER //
 CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoriaNome VARCHAR(100))
@@ -35,3 +35,26 @@ DELIMITER ;
 
 -- Teste a stored procedure
 CALL sp_ContarLivrosPorCategoria('Romance');
+
+4)Verificação de Livros por Categoria:
+
+DELIMITER //
+CREATE PROCEDURE sp_VerificarLivrosCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    DECLARE categoriaCount INT;
+    SELECT COUNT(*) INTO categoriaCount
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+
+    IF categoriaCount > 0 THEN
+        SELECT 'Categoria possui livros.' AS Status;
+    ELSE
+        SELECT 'Categoria não possui livros.' AS Status;
+    END IF;
+END //
+DELIMITER ;
+
+-- Teste a stored procedure
+CALL sp_VerificarLivrosCategoria('Romance');
+CALL sp_VerificarLivrosCategoria('Esportes');

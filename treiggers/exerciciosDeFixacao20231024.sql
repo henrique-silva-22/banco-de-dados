8df7c564af2afde5d7d78239225b28611dfe28c1
henrique-silva-22 <102774904+henrique-silva-22@users.noreply.github.com>
2023-10-24 23:13:48 -0300

    Update exerciciosDeFixacao20231024.sql

--- treiggers/exerciciosDeFixacao20231024.sql
+++ treiggers/exerciciosDeFixacao20231024.sql
@@ -1,1 +1,13 @@
+CREATE TRIGGER ad_cliente AFTER INSERT ON Clientes
+for each row INSERT INTO Auditoria (mensagem) VALUES ("Novo cliente inserido em " + NOW());
+ 
+CREATE TRIGGER aviso_d_exclusao BEFORE DELETE ON Clientes
+FOR EACH ROW INSERT INTO Auditoria (mensagem) VALUES("Tentativa de exclusão de cliente");
+ 
+CREATE TRIGGER ATUALIZACAO AFTER UPDATE ON Clientes
+FOR EACH ROW INSERT INTO Auditoria (mensagem) VALUES(CONCAT('Nome antigo: ', OLD.nome, '; Novo nome: ', NEW.nome));
 
+CREATE TRIGGER no_update BEFORE UPDATE on Clientes FOR EACH ROW
+IF NEW.nome IS NULL OR NEW.nome = "" THEN
+SIGNAL SQLSTATE '45000' SET
+INSERT INTO Auditoria (mensagem) VALUES("Tentativa de UPDATE em valor null");
